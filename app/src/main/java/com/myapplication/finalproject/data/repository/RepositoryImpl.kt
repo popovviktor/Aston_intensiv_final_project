85d246920578a2a53cfece8c95a4430b666c5801
Viktor <vincet1998@gmail.com>
2023-06-08 16:10:06 +0300

    add save db chararcters

--- app/src/main/java/com/myapplication/finalproject/data/repository/RepositoryImpl.kt
+++ app/src/main/java/com/myapplication/finalproject/data/repository/RepositoryImpl.kt
@@ -1,12 +1,8 @@
 package com.myapplication.finalproject.data.repository
 
-import android.util.Log
 import com.myapplication.finalproject.data.api.RemoteDataSource
 import com.myapplication.finalproject.domain.models.CharactersDomain
 import com.myapplication.finalproject.domain.repository.Repository
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 import javax.inject.Inject
 
 class RepositoryImpl @Inject constructor (private val remoteDataSource: RemoteDataSource):Repository {
@@ -15,11 +11,11 @@ class RepositoryImpl @Inject constructor (private val remoteDataSource: RemoteDa
         var mcharacters:MutableList<CharactersDomain>? = null
         try {
             remoteDataSource.getCharacters()
-            println("DCTTTT GOOOOOD")
+            characters = remoteDataSource.getCharacters().body()
         }catch (ex:java.lang.Exception){
             println(ex.message)
         }
-        println(characters)
+
         return characters
     }
     override fun saveDataCharactersInDb(charactersDomain: CharactersDomain){
